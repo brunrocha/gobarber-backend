507baeca1522a04cdf17b204e080f9efeb0069b0
Bruno Silva_CONTR <c0603070@vale.com>
2020-11-05 07:39:56 -0200

    Added routes and appointments routes structure

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,8 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello World' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => {
   console.log('Server started on port 3000!');
